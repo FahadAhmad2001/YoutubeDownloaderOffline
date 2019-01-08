0563e3a06906a6d26e2b6f9421914da6c99b0565
Fahad Suhail Ahmad <fahadahmad2001@outlook.com>
2019-01-08 20:44:40 +0300

    Fixed some bugs

--- YoutubeDownloaderOffline/Form1.Designer.vb
+++ YoutubeDownloaderOffline/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
@@ -40,6 +41,7 @@ Partial Class Form1
         Me.Label8 = New System.Windows.Forms.Label()
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -48,25 +50,28 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(134, 12)
+        Me.Label1.Location = New System.Drawing.Point(179, 15)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(380, 29)
+        Me.Label1.Size = New System.Drawing.Size(465, 36)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "LightSpeed YouTube Downloader"
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(523, 12)
+        Me.ProgressBar1.Location = New System.Drawing.Point(697, 15)
+        Me.ProgressBar1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ProgressBar1.Maximum = 1000
         Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(156, 23)
+        Me.ProgressBar1.Size = New System.Drawing.Size(208, 28)
         Me.ProgressBar1.TabIndex = 1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(25, 12)
+        Me.Button1.Location = New System.Drawing.Point(33, 15)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(92, 23)
+        Me.Button1.Size = New System.Drawing.Size(123, 28)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Update"
         Me.Button1.UseVisualStyleBackColor = True
@@ -75,34 +80,38 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(46, 64)
+        Me.Label2.Location = New System.Drawing.Point(61, 79)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(99, 24)
+        Me.Label2.Size = New System.Drawing.Size(126, 29)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Enter Link:"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(183, 68)
+        Me.TextBox1.Location = New System.Drawing.Point(244, 84)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(322, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(428, 22)
         Me.TextBox1.TabIndex = 4
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(220, 138)
+        Me.Label3.Location = New System.Drawing.Point(293, 170)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(99, 24)
+        Me.Label3.Size = New System.Drawing.Size(126, 29)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Enter Link:"
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(25, 108)
+        Me.PictureBox1.Location = New System.Drawing.Point(33, 133)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(172, 91)
+        Me.PictureBox1.Size = New System.Drawing.Size(229, 112)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 6
         Me.PictureBox1.TabStop = False
@@ -111,34 +120,38 @@ Partial Class Form1
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(64, 216)
+        Me.Label4.Location = New System.Drawing.Point(85, 266)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(133, 24)
+        Me.Label4.Size = New System.Drawing.Size(174, 29)
         Me.Label4.TabIndex = 7
         Me.Label4.Text = "Total File Size:"
         '
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(77, 279)
+        Me.ComboBox1.Location = New System.Drawing.Point(103, 343)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(160, 21)
+        Me.ComboBox1.Size = New System.Drawing.Size(212, 24)
         Me.ComboBox1.TabIndex = 8
         '
         'TrackBar1
         '
-        Me.TrackBar1.Location = New System.Drawing.Point(449, 279)
+        Me.TrackBar1.Location = New System.Drawing.Point(599, 343)
+        Me.TrackBar1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TrackBar1.Name = "TrackBar1"
-        Me.TrackBar1.Size = New System.Drawing.Size(148, 45)
+        Me.TrackBar1.Size = New System.Drawing.Size(197, 56)
         Me.TrackBar1.TabIndex = 9
         '
         'Label5
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(117, 255)
+        Me.Label5.Location = New System.Drawing.Point(156, 314)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(127, 24)
+        Me.Label5.Size = New System.Drawing.Size(162, 29)
         Me.Label5.TabIndex = 10
         Me.Label5.Text = "Video Quality:"
         '
@@ -146,9 +159,10 @@ Partial Class Form1
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(470, 255)
+        Me.Label6.Location = New System.Drawing.Point(627, 314)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(127, 24)
+        Me.Label6.Size = New System.Drawing.Size(161, 29)
         Me.Label6.TabIndex = 11
         Me.Label6.Text = "Audio Quality:"
         '
@@ -156,17 +170,19 @@ Partial Class Form1
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(12, 339)
+        Me.Label7.Location = New System.Drawing.Point(16, 417)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(126, 24)
+        Me.Label7.Size = New System.Drawing.Size(161, 29)
         Me.Label7.TabIndex = 12
         Me.Label7.Text = "Downloading:"
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(588, 320)
+        Me.Button2.Location = New System.Drawing.Point(784, 394)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(91, 40)
+        Me.Button2.Size = New System.Drawing.Size(121, 49)
         Me.Button2.TabIndex = 13
         Me.Button2.Text = "Download"
         Me.Button2.UseVisualStyleBackColor = True
@@ -175,9 +191,10 @@ Partial Class Form1
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(511, 64)
+        Me.Label8.Location = New System.Drawing.Point(681, 79)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(165, 24)
+        Me.Label8.Size = New System.Drawing.Size(213, 29)
         Me.Label8.TabIndex = 14
         Me.Label8.Text = "Getting Metadata..."
         '
@@ -189,18 +206,24 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(16, 313)
+        Me.Button3.Location = New System.Drawing.Point(21, 385)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(92, 23)
+        Me.Button3.Size = New System.Drawing.Size(123, 28)
         Me.Button3.TabIndex = 15
         Me.Button3.Text = "Settings"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(691, 372)
+        Me.ClientSize = New System.Drawing.Size(921, 458)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Button2)
@@ -218,6 +241,7 @@ Partial Class Form1
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Label1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.Text = "LightSpeed YouTube Downloader"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -244,4 +268,5 @@ Partial Class Form1
     Friend WithEvents Label8 As Label
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
     Friend WithEvents Button3 As Button
+    Friend WithEvents NotifyIcon1 As NotifyIcon
 End Class
