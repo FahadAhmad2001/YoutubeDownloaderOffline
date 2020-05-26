ac2a1ef7c7897bae7053540b0e81df50bae42c03
Fahad Suhail Ahmad <fahadahmad2001@outlook.com>
2020-05-26 10:57:09 +0300

    Added dependencies into project rather than installing them manually everytime

---
+++ Dependencies/EmptyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependencies
+{
+    class EmptyClass
+    {
+        public static void Main()
+        {
+            //have to create this method otherwise it wont build
+        }
+    }
+}
